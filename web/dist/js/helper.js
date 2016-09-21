6ed05833a4bad5aee2fdd58e7d1bf46580bd6164
Andry <andry.yosua@pinteraktif.com>
2016-09-21 18:59:08 +0700

    compile src

--- web/dist/js/helper.js
+++ web/dist/js/helper.js
@@ -5,66 +5,124 @@ var _createClass = function () { function defineProperties(target, props) { for 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var delay = function () {
-  var timer = 0;
-  return function (callback, ms) {
-    var clear = arguments.length <= 2 || arguments[2] === undefined ? false : arguments[2];
+    var timer = 0;
+    return function (callback, ms) {
+        var clear = arguments.length <= 2 || arguments[2] === undefined ? false : arguments[2];
 
-    if (clear) {
-      clearTimeout(timer);
-    }
-    timer = setTimeout(callback, ms);
-  };
-}();
-
-var TemplateParticipant = function () {
-  function TemplateParticipant() {
-    _classCallCheck(this, TemplateParticipant);
-  }
-
-  _createClass(TemplateParticipant, null, [{
-    key: 'get',
-    value: function get(args) {
-      return '<div class="participant">\n<div class="participant__picture"><div class="image"><img src="' + args.picture + '" class="img"/></div> </div>\n<div class="participant__name">' + args.name + '</div>\n<div class="participant__email">' + args.email + '</div>\n</div>';
-    }
-  }]);
-
-  return TemplateParticipant;
+        if (clear) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(callback, ms);
+    };
 }();
 
 var Template = function () {
-  function Template() {
-    _classCallCheck(this, Template);
-  }
-
-  _createClass(Template, null, [{
-    key: 'flash',
-    value: function flash(status, text) {
-      var className = status ? 'parsley-success-list' : 'parsley-errors-list';
-      return '<ul class="' + className + ' filled">\n    <li>' + text + '</li>\n</ul>';
+    function Template() {
+        _classCallCheck(this, Template);
     }
-  }]);
 
-  return Template;
+    _createClass(Template, null, [{
+        key: 'flash',
+        value: function flash(status, text) {
+            var className = status ? 'parsley-success-list' : 'parsley-errors-list';
+            return '<ul class="' + className + ' filled">\n    <li>' + text + '</li>\n</ul>';
+        }
+    }]);
+
+    return Template;
 }();
 
 var TemplateForm = function () {
-  function TemplateForm() {
-    _classCallCheck(this, TemplateForm);
-  }
-
-  _createClass(TemplateForm, null, [{
-    key: 'notOpen',
-    value: function notOpen() {
-      var date = arguments.length <= 0 || arguments[0] === undefined ? '' : arguments[0];
-
-      return '<p>registration will be open on ' + date + '</p>';
+    function TemplateForm() {
+        _classCallCheck(this, TemplateForm);
     }
-  }, {
-    key: 'open',
-    value: function open() {
-      return ' <h4>Registration</h4> \n<form method="POST" action="/registration" id="form_registration" enctype="multipart/form-data"> \n    <div class="form__row">\n      <div class="form__label form--required">\n        <label for="register_name">name</label>\n      </div>\n      <div class="form__field">\n        <input type="text" name="name" id="register_name">\n      </div>\n    </div>\n    <div class="form__row">\n      <div class="form__label form--required">\n        <label for="register_email">email</label>\n      </div>\n      <div class="form__field">\n        <input type="email" name="email" id="register_email" >\n      </div>\n    </div>\n    <div class="form__row">\n      <div class="form__label form--required">\n        <label for="register_dob">dob</label>\n      </div>\n      <div class="form__field">\n        <input type="text" name="dob" id="register_dob" >\n      </div>\n    </div>\n    <div class="form__row">\n      <div class="form__label form--required">\n        <label>picture</label>\n      </div>\n      <div class="form__field">\n        <label class="form__upload" for="register_picture"><span>upload</span>\n          <input type="file" name="picture" id="register_picture" accept="image/*" >\n        </label>\n      </div>\n    </div>\n    <div class="form__row">\n      <div class="form__label">\n        <label for="register_address">address</label>\n      </div>\n      <div class="form__field">\n        <textarea name="address" id="register_address"></textarea>\n      </div>\n    </div>\n    <div class="form__row">\n      <div class="form__label">\n        <label for="register_coffeeshop">coffeeshop</label>\n      </div>\n      <div class="form__field">\n        <div id="map"></div>\n        <input type="text" name="" id="register_coffeeshop">\n        <input type="hidden" name="coffeeshop_location" id="register_coffeeshop_maps">\n      </div>\n    </div>\n    <div class="form__row">\n      <div class="form__button">\n        <input type="submit" value="submit"/>\n      </div>\n    </div>\n</form>';
-    }
-  }]);
 
-  return TemplateForm;
+    _createClass(TemplateForm, null, [{
+        key: 'notOpen',
+        value: function notOpen() {
+            var date = arguments.length <= 0 || arguments[0] === undefined ? '' : arguments[0];
+
+            return '<p align="center">Open on <br/>' + date + '</p>';
+        }
+    }]);
+
+    return TemplateForm;
 }();
+
+// init maps
+
+
+var initAutocomplete = function initAutocomplete() {
+    // var map = new google.maps.Map(document.getElementById('map'), {
+    //     center: {lat: -6.187210, lng: 106.487706},
+    //     zoom: 10,
+    //     mapTypeId: 'roadmap'
+    // });
+
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('register_coffeeshop');
+    var searchBox = new google.maps.places.SearchBox(input);
+
+    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    // map.addListener('bounds_changed', function () {
+    //     searchBox.setBounds(map.getBounds());
+    // });
+
+    // var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function () {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        // markers.forEach(function (marker) {
+        //     marker.setMap(null);
+        // });
+        // markers = [];
+
+        // For each place, get the icon, name and location.
+        // var bounds = new google.maps.LatLngBounds();
+        places.forEach(function (place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            console.log(place);
+            $('#register_coffeeshop_maps').val(JSON.stringify({
+                name: place.name,
+                lat: place.geometry.location.lat(),
+                lng: place.geometry.location.lng()
+            }));
+
+            // markers.push(new google.maps.Marker({
+            //     map: map,
+            //     icon: icon,
+            //     title: place.name,
+            //     position: place.geometry.location
+            // }));
+
+            // if (place.geometry.viewport) {
+            //     // Only geocodes have viewport.
+            //     bounds.union(place.geometry.viewport);
+            // } else {
+            //     bounds.extend(place.geometry.location);
+            // }
+        });
+        // map.fitBounds(bounds);
+    });
+};
--- web/dist/js/main.js
+++ web/dist/js/main.js
@@ -9,99 +9,22 @@ if (isProd) {
 }
 urlBase = 'http://' + urlBase;
 
-// init maps
-var initAutocomplete = function initAutocomplete() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -6.187210, lng: 106.487706 },
-        zoom: 10,
-        mapTypeId: 'roadmap'
-    });
-
-    // Create the search box and link it to the UI element.
-    var input = document.getElementById('register_coffeeshop');
-    var searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener('bounds_changed', function () {
-        searchBox.setBounds(map.getBounds());
-    });
-
-    var markers = [];
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
-    searchBox.addListener('places_changed', function () {
-        var places = searchBox.getPlaces();
-
-        if (places.length == 0) {
-            return;
-        }
-
-        // Clear out the old markers.
-        markers.forEach(function (marker) {
-            marker.setMap(null);
-        });
-        markers = [];
-
-        // For each place, get the icon, name and location.
-        var bounds = new google.maps.LatLngBounds();
-        places.forEach(function (place) {
-            if (!place.geometry) {
-                console.log("Returned place contains no geometry");
-                return;
-            }
-            var icon = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25)
-            };
-
-            // Create a marker for each place.
-            console.log(place);
-            $('#register_coffeeshop_maps').val(JSON.stringify({
-                name: place.name,
-                lat: place.geometry.location.lat(),
-                lng: place.geometry.location.lng()
-            }));
-
-            markers.push(new google.maps.Marker({
-                map: map,
-                icon: icon,
-                title: place.name,
-                position: place.geometry.location
-            }));
-
-            if (place.geometry.viewport) {
-                // Only geocodes have viewport.
-                bounds.union(place.geometry.viewport);
-            } else {
-                bounds.extend(place.geometry.location);
-            }
-        });
-        map.fitBounds(bounds);
-    });
-};
-
 var getRegistrationForm = function getRegistrationForm() {
     var isTime = false;
-    var element = $('#section6 .fp-tableCell');
-    var html = void 0;
+    var element = $('#section2 .fp-tableCell');
+    var html = void 0,
+        html2 = void 0;
 
     $.ajax({
         url: urlBase + '/helper/time',
+        dataType: 'json',
         beforeSend: function beforeSend() {
-            element.find('.parsley-errors-list.filled').remove().end().addClass('spinner');
+            element.find('.parsley-errors-list.filled').remove().end();
         },
         success: function success(data) {
-            data = JSON.parse(data);
-
             isTime = data.registrationOpen;
-            if (isTime) {
-                html = TemplateForm.open();
-            } else {
-                html = data.registration.start + ' - ' + data.registration.end;
+            if (!isTime) {
+                html = data.registration.start.split(' ')[0] + ' - ' + data.registration.end.split(' ')[0];
                 html = TemplateForm.notOpen(html);
             }
         },
@@ -109,125 +32,109 @@ var getRegistrationForm = function getRegistrationForm() {
             html = Template.flash(false, xhr.statusText);
         },
         complete: function complete() {
-            element.removeClass('spinner');
+            element.find('.mdl-progress').hide();
 
-            var $form = element.find('#form_registration');
-            console.log(!isTime, isTime && $form.length == 0);
-            if (!isTime || isTime && $form.length == 0) {
-                element.empty().append(html);
+            var $form = element.find('form');
+
+            if (!isTime) {
+                element.find('.mdl-card__supporting-text').empty().append(html);
                 $form = element.find('#form_registration');
-                if ($form.length > 0) {
-                    $form.find("#register_dob").datepicker({
-                        changeMonth: true,
-                        changeYear: true,
-                        minDate: '-80y',
-                        maxDate: '-5y',
-                        yearRange: "-100:-5",
-                        dateFormat: 'yy-mm-dd',
-                        onClose: function onClose(dateText, inst) {
-                            var $this = inst.input;
-                            console.log($this);
-                            $this.trigger('leave');
-                        }
-                    });
-
-                    $form.on('change', '#register_picture', function (e) {
-                        var $this = $(e.target);
-                        var $parent = $this.closest('.form__field');
-                        var input = $this[0];
-
-                        if (input.files && input.files[0]) {
-                            var reader = new FileReader();
-
-                            reader.onload = function (e) {
-                                var $img = $parent.find('img.form__upload-image');
-                                if ($img.length === 0) {
-                                    $parent.prepend('<img class="form__upload-image"/>');
-                                    $img = $parent.find('img');
-                                }
-                                $img.attr('src', e.target.result);
-                                $.fn.fullpage.reBuild();
-                            };
-
-                            reader.readAsDataURL(input.files[0]);
-                        }
-                    });
-
-                    $form.on('submit', function (e) {
-                        e.preventDefault();
-
-                        var $this = $(e.target);
-
-                        // $this.parsley().validate();
-                        //
-                        // if (!$this.parsley().isValid()) {
-                        //     return false
-                        // }
-                        var formData = new FormData($this[0]);
-                        var $buttonContainer = $this.find('.form__button');
-                        var $buttonSubmit = $buttonContainer.find('[type="submit"]');
-                        var html = void 0;
-
-                        $.ajax({
-                            url: '' + urlBase + $this.attr('action'),
-                            type: $this.attr('method'),
-                            data: formData,
-                            //async: false,
-                            beforeSend: function beforeSend() {
-                                $buttonSubmit.attr('disabled', true).addClass('spinner');
-                            },
-                            success: function success(data) {
-                                data = JSON.parse(data);
-
-                                html = Template.flash(data.status, data.message);
-                            },
-                            error: function error(xhr) {
-                                html = Template.flash(false, xhr.statusText);
-                            },
-                            complete: function complete() {
-                                $buttonContainer.find('.parsley-errors-list, .parsley-success-list').remove().end().prepend(html);
-
-                                $buttonSubmit.prop('disabled', false).removeClass('spinner');
-                            },
-
-                            cache: false,
-                            contentType: false,
-                            processData: false
-                        });
-
-                        return false;
-                    });
-
-                    $('body').append('<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBjhGHb81MIdRsBEJhhSSUe78JLGIRhHJA&libraries=places&callback=initAutocomplete"></script>');
-                    $.fn.fullpage.reBuild();
-                }
             }
-        }
-    });
-};
 
-var getParticipants = function getParticipants() {
-    var element = $('#section2 .fp-tableCell');
-    var html = void 0;
-    $.ajax({
-        url: urlBase + '/participant',
-        beforeSend: function beforeSend() {
-            element.find('.parsley-errors-list.filled').remove().end().addClass('spinner');
-        },
-        success: function success(data) {
-            data = JSON.parse(data);
-            html = data.items.map(function (v) {
-                return '<li>' + TemplateParticipant.get(v) + '</li>';
-            }).join('');
+            if ($form.length === 0) {
+                element.find('.mdl-card__actions.mdl-card--border').remove();
+            } else {
+                $form.find("#register_dob").datepicker({
+                    changeMonth: true,
+                    changeYear: true,
+                    minDate: '-80y',
+                    maxDate: '-5y',
+                    yearRange: "-100:-5",
+                    dateFormat: 'yy-mm-dd',
+                    onSelect: function onSelect(dateText, inst) {
+                        var $this = inst.input;
+                        // console.log($this);
+                        // $this.trigger('leave');
+                        // $(this).valid()
+                        $this.closest('.mdl-textfield').removeClass('is-invalid').addClass('is-dirty');
+                    }
+                });
 
-            html = '<ul class="participants">' + html + '</ul>';
-        },
-        error: function error(xhr) {
-            html = Template.flash(false, xhr.statusText);
-        },
-        complete: function complete() {
-            element.removeClass('spinner').empty().append(html).find('.participant img').pictureOrientation();
-            $.fn.fullpage.reBuild();
+                $form
+                // .off('change', '#register_dob')
+                // .on('change', '#register_dob', (e)=> {
+                //     console.log($(this))
+                // })
+                .on('change', '#register_picture', function (e) {
+                    var $this = $(e.target);
+                    var $imgEl = $this.closest('.mdl-card').find('.mdl-card__title');
+                    var input = $this[0];
+                    console.log($imgEl);
+
+                    if (input.files && input.files[0]) {
+                        var reader = new FileReader();
+
+                        reader.onload = function (e) {
+                            $imgEl.css('background-image', 'url(' + e.target.result + ')');
+                            $.fn.fullpage.reBuild();
+                        };
+
+                        reader.readAsDataURL(input.files[0]);
+                    }
+                });
+
+                element.off('click', '.mdl-card__actions button').on('click', '.mdl-card__actions button', function (e) {
+                    $form.trigger('submit');
+                });
+
+                $form.off('submit').on('submit', function (e) {
+                    e.preventDefault();
+
+                    var $this = $(e.target);
+
+                    var formData = new FormData($this[0]);
+                    var $buttonContainer = element.find('.mdl-card__supporting-text');
+                    var $buttonSubmit = element.find('.mdl-card__actions button');
+
+                    $.ajax({
+                        url: '' + urlBase + $this.attr('action'),
+                        dataType: 'json',
+                        type: $this.attr('method'),
+                        data: formData,
+                        //async: false,
+                        beforeSend: function beforeSend() {
+                            $buttonSubmit.attr('disabled', true);
+
+                            element.find('.parsley-errors-list.filled').remove().end().find('.mdl-progress').show();
+                        },
+                        success: function success(data) {
+                            html2 = Template.flash(data.status, data.message);
+                            element.find('[name=' + data.element + ']').closest('.mdl-textfield').addClass('is-invalid');
+                        },
+                        error: function error(xhr) {
+                            html2 = Template.flash(false, xhr.statusText);
+                        },
+                        complete: function complete() {
+                            $buttonContainer.append(html2);
+                            $buttonSubmit.attr('disabled', false);
+                            element.find('.mdl-progress').hide();
+                        },
+
+                        cache: false,
+                        contentType: false,
+                        processData: false
+                    });
+
+                    return false;
+                });
+
+                $('body').append('<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBjhGHb81MIdRsBEJhhSSUe78JLGIRhHJA&libraries=places&callback=initAutocomplete"></script>');
+                var f = function f() {
+                    $('#register_coffeeshop').removeAttr('placeholder');
+                };
+                delay(f, 500);
+                $.fn.fullpage.reBuild();
+            }
         }
     });
 };
@@ -238,14 +145,11 @@ $(function () {
             case 'registration':
                 getRegistrationForm();
                 break;
-            case 'challengers':
-                getParticipants();
-                break;
         }
     };
 
     $('#fullpage').fullpage({
-        anchors: ['sponsor', 'challengers', 'rules', 'schedule', 'venue', 'registration'],
+        anchors: ['main', 'registration'],
         // menu: 'nav.menu ul',
         scrollOverflow: true,
         // scrollOverflowOptions: {
